5b4199027a4455e543ae2ddce62a65968cb4ae9c
Ambar Maini <ambarmaini1993@gmail.com>
2017-09-26 13:52:05 +0000

    Added executable file

--- com.parkingLot/src/main/java/parkingLot/com/parkingLot/App.java
+++ com.parkingLot/src/main/java/parkingLot/com/parkingLot/App.java
@@ -12,6 +12,72 @@ import java.util.Scanner;
  *
  */
 public class App {
+
+	public static void display(String input,ParkingLotUtil putil)
+	{
+		String inputs[] = input.split(" ");
+		if (inputs[0].equals("create_parking_lot")) {
+			if (putil.createParkingLot(Integer.parseInt(inputs[1]))) {
+				System.out.println("Created a parking lot with " + inputs[1] + " slots");
+
+			} else {
+				System.out.println("Invalid slot size");
+			}
+
+		} else if (inputs[0].equals("park")) {
+			String regno = inputs[1];
+			String color = inputs[2];
+			int slotid = putil.getMinimumSlot();
+			if (slotid == -1 && putil.slots == null) {
+				System.out.println("Sorry, create a parking lot first");
+
+			} else if (slotid == -1) {
+				System.out.println("Sorry, parking lot is full");
+			} else {
+				if (putil.Allot(color, regno, slotid))
+					System.out.println("Allocated slot number: " + slotid);
+
+			}
+		} else if (inputs[0].equals("leave")) {
+			int slotId = Integer.parseInt(inputs[1]);
+			if (putil.Leave(slotId))
+				System.out.println("Slot number " + slotId + " is free");
+			else
+				System.out.println("Not found");
+		} else if (inputs[0].equals("status")) {
+
+			putil.status();
+		} else if (inputs[0].equals("registration_numbers_for_cars_with_colour")) {
+			List<String> listreg = putil.getListRegNo(inputs[1]);
+			StringBuffer sb = new StringBuffer();
+			if (!listreg.isEmpty()) {
+				for (String l : listreg) {
+					sb.append(l + ", ");
+				}
+				System.out.println(sb.substring(0, sb.length() - 2));
+			} else {
+				System.out.println("No car is there for " + inputs[1] + " color");
+			}
+		} else if (inputs[0].equals("slot_numbers_for_cars_with_colour")) {
+			List<Integer> listnum = putil.getSlots(inputs[1]);
+			StringBuffer sb = new StringBuffer();
+			for (Integer l : listnum) {
+				sb.append(l + ", ");
+			}
+			System.out.println(sb.substring(0, sb.length() - 2));
+
+		} else if (inputs[0].equals("slot_number_for_registration_number")) {
+			int slotid = putil.getSlotId(inputs[1]);
+			if (slotid == -1)
+				System.out.println("Not found");
+			else
+				System.out.println(slotid);
+		}
+
+	}
+
+
+
 	public static void main( String[] args )
     {
     	BufferedReader br = null;
@@ -19,101 +85,35 @@ public class App {
 
     	  try {
 
-    	 //  Scanner sc = new Scanner(System.in);
+    	  	ParkingLotUtil putil = new ParkingLotUtil();
+			if(args.length != 0) {
 
-    	   String file = File.separator+"home"+File.separator+"ubuntu"+File.separator+"Desktop"+File.separator+"parkingLot"+File.separator+"input.txt";
+				String file = "..//" + args[0];
+				//String file = File.separator+"home"+File.separator+"ubuntu"+File.separator+"Desktop"+File.separator+"parkingLot"+File.separator+"input.txt";
+				fr = new FileReader(file);
+				br = new BufferedReader(fr);
 
-			  fr = new FileReader(file);
-    	   br = new BufferedReader(fr);
+				String sCurrentLine;
 
-    	   String sCurrentLine;
 
-    	   ParkingLotUtil putil=new ParkingLotUtil();
-    	   while ((sCurrentLine = br.readLine()) != null) {
-    		   String inputs[]= sCurrentLine.split(" ");
-    		   if(inputs[0].equals("create_parking_lot"))
-    		   {
-    			   if(putil.createParkingLot(Integer.parseInt(inputs[1]))){
-    				   System.out.println("Created a parking lot with "+inputs[1]+" slots");
+				while ((sCurrentLine = br.readLine()) != null) {
+					display(sCurrentLine,putil);
 
-    			   }
-    			   else
-    			   {
-    				   System.out.println("Invalid slot size");
-    			   }
-    			   
-    		   }
-    		   else if(inputs[0].equals("park"))
-    		   {
-    			   String regno = inputs[1];
-    			   String color = inputs[2];
-    			   int slotid = putil.getMinimumSlot();
-    			   if(slotid==-1 && putil.slots==null)
-    			   {
-    				   System.out.println("Sorry, create a parking lot first");
+				}
+			}
 
-    			   }else if(slotid == -1 )
-    			   {
-    				   System.out.println("Sorry, parking lot is full");
-    			   }
-    			   else 
-    			   {
-    				   if(putil.Allot(color, regno,slotid ))
-    				   System.out.println("Allocated slot number: "+slotid);
-    					   
-    			   }
-    		   }
-    		   else if(inputs[0].equals("leave"))
-    		   {
-    			   int slotId = Integer.parseInt(inputs[1]);
-    			   if(putil.Leave(slotId))
-    				   System.out.println("Slot number "+slotId+" is free");
-    			   else
-    				   System.out.println("Not found");
-    		   }
-    		   else if(inputs[0].equals("status"))
-    		   {
-    			   
-    			   putil.status();
-    		   }
-    		   else if(inputs[0].equals("registration_numbers_for_cars_with_colour"))
-    		   {
-    			   List<String> listreg = putil.getListRegNo(inputs[1]);
-    			   StringBuffer sb = new StringBuffer();
-    			   if(!listreg.isEmpty()){
-    			   for(String l : listreg)
-    			   {
-    				   sb.append(l+", ");
-    			   }
-    			   System.out.println(sb.substring(0, sb.length()-2));
-    			   }
-    			   else{
-    				   System.out.println("No car is there for "+inputs[1] +" color");
-    			   }
-    		   }
-    		   else if(inputs[0].equals("slot_numbers_for_cars_with_colour"))
-    		   {
-    			   List<Integer> listnum = putil.getSlots(inputs[1]);
-    			   StringBuffer sb = new StringBuffer();
-    			   for(Integer l : listnum)
-    			   {
-    				   sb.append(l+", ");
-    			   }
-    			   System.out.println(sb.substring(0, sb.length()-2));
-    			  
-    		   }
-    		   else if(inputs[0].equals("slot_number_for_registration_number"))
-    		   {
-    			   int slotid = putil.getSlotId(inputs[1]);
-    			   if(slotid == -1)
-    				   System.out.println("Not found");
-    			   else
-    				   System.out.println(slotid);
-    		   }
-    			   
-    		   
-    		   
-    	   }
+			else
+			{
+
+				Scanner sc = new Scanner(System.in);
+				while(sc.hasNext())
+				{
+					String input = sc.nextLine();
+					display(input , putil);
+
+				}
+			}
+
 
     	  } catch (IOException e) {
 
@@ -137,4 +137,5 @@ public class App {
     }
     
 }
+
 }
